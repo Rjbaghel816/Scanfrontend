e0a79a9f0de8e98d9872275afc0ce39a234686bd
Rajesh <rajeshbaghel2425@gmail.com>
2025-11-17 12:49:21 +0530

    add connection

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://scanbackend-4l4e.onrender.com/api';
+const API_BASE = 'https://scanbackend-4l4e.onrender.com';
 
 class ApiService {
   async request(endpoint, options = {}) {
